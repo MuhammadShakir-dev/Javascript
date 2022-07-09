b4d7bb7cf8bcb5a069e33bbec2f2cdc8180da7f5
Muhammad Shakir <84896803+MuhammadShakir-dev@users.noreply.github.com>
2022-07-09 12:35:36 +0500

    Create evenOdd.js

---
+++ evenOdd.js
@@ -0,0 +1,16 @@
+<script>
+      /*
+                    EVEN AND ODD NUMBER DETATCTION IN JAVASCRIPT.
+
+          
+        */
+      function evenOdd() {
+        var a = prompt("Enter a number");
+        if (a % 2 == 0) {
+          alert("This is an even number");
+        } else {
+          alert("This is an odd number");
+        }
+      }
+      evenOdd();
+    </script>
